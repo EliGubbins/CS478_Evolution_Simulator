a6cd69bdc57a79bc841ccdfcc24694a2dfa23ae8
acforche <aforky11@gmail.com>
2026-04-20 12:22:31 -0500

    Changed the background to be green to simulate grass

--- Rendering/RenderColor.cs
+++ Rendering/RenderColor.cs
@@ -3,6 +3,7 @@ namespace EvolutionSimulator.Core.Rendering
     public readonly record struct RenderColor(byte R, byte G, byte B, byte A = 255)
     {
         public static readonly RenderColor ForestGreen = new(74, 140, 84);
+        public static readonly RenderColor PastelGrass = new(196, 226, 184);
         public static readonly RenderColor SunsetOrange = new(217, 91, 67);
         public static readonly RenderColor Wheat = new(232, 211, 162);
         public static readonly RenderColor Slate = new(70, 78, 92);
